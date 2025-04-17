dc0372b6d12d78b6397a1e2bcff689100455e83e
Juhasen <juszczyk-krystian@wp.pl>
2025-04-17 19:26:05 +0200

    Added color and font definitions to tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+﻿module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                quiz: ['"Fredoka"', 'sans-serif'],
+            },
+            colors: {
+                quizPink: '#FF7EB9',
+                quizYellow: '#FEE440',
+                quizGreen: '#00F5D4',
+                quizBlue: '#5BC0EB',
+            },
+        },
+    },
+    plugins: [],
+};
