3373ea336ec796515edffdff9a9b5f89c2a11b41
Juhasen <juszczyk-krystian@wp.pl>
2025-05-15 23:44:02 +0200

    Created Header and Footer

---
+++ src/app/[locale]/components/Footer.tsx
@@ -0,0 +1,14 @@
+﻿
+
+export default function Footer() {
+    return (
+        <>
+            </>
+        // <footer className="bg-gray-800 text-white py-4">
+        //     <div className="container mx-auto text-center">
+        //         <p className="text-sm">© 2023 Quizownik. All rights reserved.</p>
+        //         <p className="text-sm">Made with ❤️ by Quizownik Team</p>
+        //     </div>
+        // </footer>
+    );
+}
---
+++ src/app/[locale]/components/Header.tsx
@@ -0,0 +1,29 @@
+﻿"use client"
+import {Link} from "@/i18n/navigation";
+import {useState} from "react";
+import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
+
+export default function Header() {
+    const [isLoggedIn] = useState(false);
+
+    if (!isLoggedIn) {
+        return null;
+    }
+    return (
+        <header className="grid grid-cols-3 w-full items-center justify-center p-4  text-white">
+            <h1 className="text-2xl font-bold text-quizPink">Quizownik</h1>
+            <nav className="flex items-center justify-center">
+                <ul className="flex space-x-4">
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/quizzes">Quizzes</Link></li>
+                    <li><Link href="/community">Community</Link></li>
+                </ul>
+            </nav>
+
+            <Link href="/profile" className="flex items-center justify-end text-white px-4 py-2 rounded">
+                <AccountCircleTwoToneIcon fontSize={"large"} />
+            </Link>
+        </header>
+    );
+
+}
