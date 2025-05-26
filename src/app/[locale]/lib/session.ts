a51a20fa74e9d68abdd67b4998732e92744c5202
Juhasen <juszczyk-krystian@wp.pl>
2025-05-27 01:27:28 +0200

    Add deleteSession function to manage session cookie removal

--- src/app/[locale]/lib/session.ts
+++ src/app/[locale]/lib/session.ts
@@ -59,3 +59,8 @@ export async function updateSession() {
         path: '/',
     })
 }
+
+export async function deleteSession() {
+    const cookieStore = await cookies()
+    cookieStore.delete('session')
+}
