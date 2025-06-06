4e2aef2756c8ca4cf80a0926cda183d900454a40
Juhasen <juszczyk-krystian@wp.pl>
2025-06-06 19:38:53 +0200

    Add borderCategoryColors and enhance CategoryChip component with border styling and icon

--- src/app/[locale]/components/categoryChip.tsx
+++ src/app/[locale]/components/categoryChip.tsx
@@ -1,16 +1,27 @@
-﻿import { categoryColors } from "@/app/[locale]/lib/categoryColors";
+﻿import {categoryColors} from "@/app/[locale]/lib/categoryColors";
+import {borderCategoryColors} from "@/app/[locale]/lib/borderCategoryColors";
 
 type CategoryChipProps = {
     name: string;
+    textToDisplay: string;
 };
 
-export default function CategoryChip({ name }: CategoryChipProps) {
-    const color = categoryColors[name] || "border-gray-400";
+export default function CategoryChip({name, textToDisplay}: CategoryChipProps) {
+    const color = categoryColors[name] || "black";
+    const borderColor = borderCategoryColors[name];
+    const iconSrc = "/icons/category.png";
 
     return (
         <span
-            className={`inline-block px-3 text-black rounded-full  text-sm border-${color} border-2 bg-${color} bg-opacity-20 lowercase`}>
-            {name}
+            className={`inline-flex items-center gap-2 py-1 px-3 text-black rounded-full text-sm ${borderColor} border-2 bg-${color} bg-opacity-30 lowercase whitespace-nowrap`}
+        >
+            <img
+                src={iconSrc}
+                alt={textToDisplay}
+                className="w-4 h-4"
+                loading="lazy"
+            />
+            {textToDisplay}
         </span>
     );
 }
---
+++ src/app/[locale]/lib/borderCategoryColors.ts
@@ -0,0 +1,7 @@
+﻿const borderCategoryColors: Record<string, string> = {
+    Grammar: "border-quizYellow",
+    Vocabulary: "border-quizBlue",
+    Mixed: "border-quizPink"
+};
+
+export {borderCategoryColors};
